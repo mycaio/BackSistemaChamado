530ba5cfa4737de761980263fe4bb60ef1ba0353
TEC- Caio Renan De Lima Mendonca <caio.mendonca@unimedlondrina.com.br>
2026-06-17 11:26:12 -0300

    Subindo criação do projeto

---
+++ src/main/java/com/example/BackSistemaChamado/BackSistemaChamadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackSistemaChamado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSistemaChamadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSistemaChamadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackSistemaChamado/BackSistemaChamadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackSistemaChamado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSistemaChamadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
